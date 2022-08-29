3707a25066951d075339ef801426882fde08fa64
last <ac7you@gmail.com>
2022-08-29 20:10:48 +0300

    Update sxhkdrc

--- cstart.sh
+++ cstart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
-xset r rate 170 40 >> ~/.xinitrc
-setxkbmap -layout us,ru -option grp:alt_shift_toggle >> ~/.xinitrc
+echo "xset r rate 170 40" >> ~/.xinitrc
+echo "setxkbmap -layout us,ru -option grp:alt_shift_toggle" >> ~/.xinitrc
